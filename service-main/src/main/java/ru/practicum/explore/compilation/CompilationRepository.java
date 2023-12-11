b7a0f2d6468c1f725ab94c0f74481104775d05c2
Piroggard <piroggard@yandex.ru>
2023-12-11 18:42:44 +0300

    Фикс форматирование, лишний код, лишний импорт

--- service-main/src/main/java/ru/practicum/explore/compilation/CompilationRepository.java
+++ service-main/src/main/java/ru/practicum/explore/compilation/CompilationRepository.java
@@ -3,7 +3,6 @@ package ru.practicum.explore.compilation;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 
 @Repository
 public interface CompilationRepository extends JpaRepository<Compilation, Long> {
