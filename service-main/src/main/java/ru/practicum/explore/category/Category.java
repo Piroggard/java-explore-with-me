782d498f80e079daffb6030fe73455d53acf6d4b
Piroggard <piroggard@yandex.ru>
2023-12-27 11:10:24 +0300

    Фикс импортов

--- service-main/src/main/java/ru/practicum/explore/category/Category.java
+++ service-main/src/main/java/ru/practicum/explore/category/Category.java
@@ -14,6 +14,7 @@ import javax.persistence.Id;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Column;
 import javax.persistence.GenerationType;
+
 @Getter
 @Setter
 @AllArgsConstructor
